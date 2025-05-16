e570bbbb510f448d2f1414700fb8ce73a9695c59
BneCaglar34 <caglarozacikgoz@gmail.com>
2025-05-16 19:34:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from sifrele import sifrele, coz
 
 if __name__ == "__main__":
-    # Şifreleme örneği
-    orjinal_metin = "aşık"
-    sifreli = sifrele(orjinal_metin)
-    print(f"Şifreli: {sifreli}")  # [2, 83, 37, 43, 3]
+    # Temel alfabe ile
+    sifreli_temel = sifrele("aşıkb", kaydirma=False)
+    print(f"Temel Şifre: {sifreli_temel}")  # [2, 83, 37, 43, 3]
 
-    # Çözme örneği
-    cozulen = coz(sifreli)
-    print(f"Çözülen: {cozulen}")  # "aşıkb"
+    # 4 kaydırmalı alfabe ile
+    sifreli_kaydirma = sifrele("aşıkb", kaydirma=True)
+    print(f"Kaydırmalı Şifre: {sifreli_kaydirma}")  # [113, 239, 179, 191, 127]
+
+    # Çözme örnekleri
+    print(coz(sifreli_temel, kaydirma=False))  # "aşıkb"
+    print(coz(sifreli_kaydirma, kaydirma=True))  # "aşıkb"
