58f3c987443dd122942897625bc0f0d406411682
BneCaglar34 <caglarozacikgoz@gmail.com>
2025-05-16 19:34:36 +0300

    Update sifrele.py

--- sifrele.py
+++ sifrele.py
@@ -1,9 +1,11 @@
-from sozluk import ASAL_ALFABE, TERS_ALFABE
+from sozluk import *
 
-def sifrele(metin: str) -> list[int]:
-    """Metni asal sayı listesine çevirir."""
-    return [ASAL_ALFABE[harf] for harf in metin.lower() if harf in ASAL_ALFABE]
+def sifrele(metin: str, kaydirma: bool = False) -> list[int]:
+    """Metni asal sayılara çevirir (4 kaydırmalı veya temel)"""
+    sozluk = KAYDIRMALI_ALFABE if kaydirma else TEMEL_ALFABE
+    return [sozluk[harf] for harf in metin.lower() if harf in sozluk]
 
-def coz(asal_liste: list[int]) -> str:
-    """Asal sayı listesini metne çevirir."""
-    return ''.join([TERS_ALFABE[sayi] for sayi in asal_liste if sayi in TERS_ALFABE])
+def coz(asal_liste: list[int], kaydirma: bool = False) -> str:
+    """Asal sayıları metne çevirir (4 kaydırmalı veya temel)"""
+    sozluk = TERS_KAYDIRMALI if kaydirma else TERS_TEMEL
+    return ''.join([sozluk[sayi] for sayi in asal_liste if sayi in sozluk])
